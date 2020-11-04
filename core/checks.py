9cf4814e6d34e5fe6dbb76cba6616f2eb14d5ad7
madprogramer <themadprogramer@gmail.com>
2020-11-04 12:32:51 +0300

    Quick Fix

--- core/checks.py
+++ core/checks.py
@@ -6,9 +6,9 @@ def CheckConfigExists():
 	#Check to see if config exists
 	assert (os.path.exists('config.json')), "config.json is missing.\nUntil we add something to generate a new config.json we are going to have to ask you to re-install.\nSorry :{"
 	#Check to see if config is loadable
-	
-	#assert (condition), "error"
 
+	#assert (condition), "error"
+	JSON = {}
 	return JSON
 
 def CheckURLPrefix(JSON):
