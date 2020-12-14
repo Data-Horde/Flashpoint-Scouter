567bee4213f7e36a6680e7e0a3d685e2746cbb8d
madprogramer <themadprogramer@gmail.com>
2020-12-14 18:26:52 +0300

    Implemented upperbound on game/animation count

--- core/crawler.py
+++ core/crawler.py
@@ -28,21 +28,52 @@ class Crawler:
         Attempt to get a URL and return status code.
         Returns None if ConnectionError
         """
-        try:
-            response = self.s.get(URL).status_code
-        except requests.exceptions.ConnectionError as e:
-            return None
+        return self.s.get(URL).status_code
 
     #ID-Based Crawl
     def CrawlById(self,prefix,suffix):
+
+        
+
+        #Determine Limit
+        LIMIT = 0
+
+        #Step 0
+        #Check 0 index
+        #IGNORE WHEN COUNTING, SINCE 0 GETS ITS OWN CHECK
+
+        #Step 1: Find the Upper Bound
+        #Check index 1 and try powers of two
+        UB = 1
+        #Introduce skips ot avoid gaps!
+        skips = 3
+        used = 0
+
         last_status_code = 200
-        #while last_status_code == 200:
-        #Don't forget 0 index (IF it does exist)
-        target = prefix+str(1)+suffix
-        self.getStatusCode(target)
+        while last_status_code == 200 or used < skips:
+
+            target = self.idURL(prefix,UB+used,suffix)
+            try:
+                last_status_code = self.getStatusCode(target)
+            except requests.exceptions.ConnectionError as e:
+                print("Lost internet connection, retrying")
+                continue
+            
+            #print(target)
+            #print(UB+used, last_status_code)
+            print(last_status_code, target)
+    
+            if last_status_code != 200:
+                used+=1
+            else:
+                UB*=2
+                used=0
+
+        print("No more than {} items!".format(str(UB)))
+
+        #Step 2: Binary Search the limit!
 
     #Attempt Crawl
-
     def AttemptCrawl(self, crawlMethod='id', urlFormat='pre+suf'):
 
         if crawlMethod == 'id' and urlFormat == 'pre+suf':
@@ -52,12 +83,6 @@ class Crawler:
 
             print("Todo: implement save/loading counts and cookies from json!")
 
-            #Determine Limit
-            LIMIT = 0
-
-            #Check 0 index
-
-            #Check 1 index and try powers of two
             results = self.CrawlById(URLPrefix,URLSuffix)
             return results
 
@@ -66,7 +91,10 @@ class Crawler:
         	return []
 
     ###########################
-    #Doesn't take self
+    #Doesn't NEED self
     ###########################
+
+    def idURL(self,prefix,id,suffix=''):
+        return prefix+str(id)+suffix
 
 
